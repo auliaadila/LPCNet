05fa2f7dfea5ecc6e8c770686962bc30c83a1a8a
auliaadila <auliaadila36@gmail.com>
2025-06-12 19:51:43 +0900

    update bits_in in dataloader.py

--- training_tf2/dataloader.py
+++ training_tf2/dataloader.py
@@ -15,7 +15,7 @@ def lpc2rc(lpc):
 BITS_PER_FRAME = 64
 
 class LPCNetLoader(Sequence):
-    def __init__(self, data, features, periods, batch_size, e2e=False, lookahead=2):
+    def __init__(self, data, features, periods, batch_size, bits_in=None, e2e=False, lookahead=2):
         self.batch_size = batch_size
         self.nb_batches = np.minimum(np.minimum(data.shape[0], features.shape[0]), periods.shape[0])//self.batch_size
         self.data = data[:self.nb_batches*self.batch_size, :]
@@ -24,11 +24,27 @@ class LPCNetLoader(Sequence):
         self.e2e = e2e
         self.lookahead = lookahead
         self.bits_per_frame = BITS_PER_FRAME
+
+        if bits_in is None:
+            self._make_random_bits()        # fills self.bits_in
+        else:
+            assert bits_in.shape[:2] == self.features.shape[:2], \
+                "bits_in must match (N,F) of features"
+            self.bits_in = bits_in.astype('int32')
+
         self.on_epoch_end()
 
+    def _make_random_bits(self):
+        N, F = self.features.shape[:2] #batch_size, nb_frames
+        self.bits_in = np.random.randint(
+            0, 2, size=(N, F, self.bits_per_frame), dtype='int32')
+        
     def on_epoch_end(self):
         self.indices = np.arange(self.nb_batches*self.batch_size)
         np.random.shuffle(self.indices)
+        # regenerate a fresh random payload each epoch (optional)
+        self._make_random_bits()
+
 
     def __getitem__(self, index):
         data = self.data[self.indices[index*self.batch_size:(index+1)*self.batch_size], :, :]
@@ -36,16 +52,8 @@ class LPCNetLoader(Sequence):
         out_data = data[: , :, 1:]
         features = self.features[self.indices[index*self.batch_size:(index+1)*self.batch_size], :, :-16]
         periods = self.periods[self.indices[index*self.batch_size:(index+1)*self.batch_size], :, :]
-        
-        # -------------- WATERMARK BITS ---------------------------
-        nb_frames = features.shape[1]          # time dimension (=T/160)
-        bits_in = np.random.randint(0, 2,
-                    size=(self.batch_size,
-                          nb_frames,
-                          self.bits_per_frame)
-                  ).astype('int32')            # shape (B,F,64)
-        
-        # -------------- pack inputs / outputs --------------------
+        bits_in = self.bits_in[self.indices[index*self.batch_size:(index+1)*self.batch_size], :, :]
+
         outputs = [out_data]
         inputs = [in_data, features, periods, bits_in]
         
